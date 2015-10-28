cb40126dc004c559d198c733559dd58281bd8039
Duhan <dcms2@cin.ufpe.br>
2015-10-28 13:15:27 -0300

    classificador pronto

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -8,7 +8,7 @@ neg_examples: [id]
 test_set: [(text, [class_names])]
 '''
 
-def BinaryClassifier:
+class BinaryClassifier:
 
   def __init__(self, id_to_text, pos_examples, neg_examples):
     self._word_freq = {'pos' : {}, 'neg' : {}}
@@ -16,12 +16,25 @@ def BinaryClassifier:
     self._prob_class = {'pos' : len(pos_examples) / (len(pos_examples) + len(neg_examples)),
                         'neg' : len(neg_examples) / (len(pos_examples) + len(neg_examples))}
     self.train(id_to_text, pos_examples, neg_examples)
-    pass
 
 
   def train(self, id_to_text, pos_examples, neg_examples):
     ''' Trains this classifier. '''
-    pass
+    for id_exmp in pos_examples:
+      for token in id_to_text[id_exmp]:
+        self._vocabulary.add(token)
+        if token in self._word_freq['pos']:
+          self._word_freq['pos'][token] += 1
+        else:
+          self._word_freq['pos'][token] = 1
+
+    for id_exmp in neg_examples:
+      for token in id_to_text[id_exmp]:
+        self._vocabulary.add(token)
+        if token in self._word_freq['neg']:
+          self._word_freq['neg'][token] += 1
+        else:
+          self._word_freq['neg'][token] = 1
 
 
   def classify(self, text):
@@ -31,10 +44,15 @@ def BinaryClassifier:
             'neg' : log(self._prob_class['neg'])}
     for token in text:
       for c in prob:
-        prob[c] += log(self._word_freq[c][token] + 1) - log(sum(self._word_freq[c].values()) + len(self._vocabulary))
+        if token in self._word_freq[c]:
+          f = self._word_freq[c][token]
+        else:
+          f = 0
+        prob[c] += log(f + 1) - log(sum(self._word_freq[c].values()) + len(self._vocabulary))
     return prob['pos'] > prob['neg']
 
-def NAryClassifier:
+
+class NAryClassifier:
 
   def __init__(self, class_to_ids, id_to_text):
     self._classifiers = {}
@@ -42,7 +60,7 @@ def NAryClassifier:
     for class_name in class_to_ids:
       self._class_names.append(class_name)
       pos_examples = class_to_ids[class_name]
-      neg_examples = [id_exmp for id_exmp in id_to_text.keys(): if id_exmp not in pos_examples]
+      neg_examples = [id_exmp for id_exmp in id_to_text.keys() if id_exmp not in pos_examples]
       self._classifiers[class_name] = BinaryClassifier(id_to_text, pos_examples, neg_examples)
 
 
@@ -57,29 +75,42 @@ def NAryClassifier:
 
   def get_metrics(self, test_set):
     ''' Returns the metrics: precision, recall, accuracy and f1, for both micro and macro averaging. '''
-    tp = {class_name: 0.0 for class_name in self._class_names}
-    fp = {class_name: 0.0 for class_name in self._class_names}
-    tn = {class_name: 0.0 for class_name in self._class_names}
-    fn = {class_name: 0.0 for class_name in self._class_names}
+    tp = {class_name: 0 for class_name in self._class_names}
+    fp = {class_name: 0 for class_name in self._class_names}
+    tn = {class_name: 0 for class_name in self._class_names}
+    fn = {class_name: 0 for class_name in self._class_names}
 
     for (text, classes) in test_set:
       for class_name, classifier in self._classifiers.items():
         if classifier.classify(text):
           if class_name in classes:
-            tp[class_name] += 1.0
+            tp[class_name] += 1
           else:
-            fp[class_name] += 1.0
+            fp[class_name] += 1
         else:
           if class_name in classes:
-            fn[class_name] += 1.0
+            fn[class_name] += 1
           else:
-            tn[class_name] += 1.0
+            tn[class_name] += 1
+
+    print("tp: " + str(tp))
+    print("fn: " + str(fn))
+    print("tn: " + str(tn))
+    print("fp: " + str(fp))
 
     # macro average
     precision, recall, accuracy = 0.0, 0.0, 0.0
     for class_name in self._class_names:
-      precision += tp[class_name] / (tp[class_name] + fp[class_name])
-      recall += tp[class_name] / (tp[class_name] + fn[class_name])
+      if tp[class_name] + fp[class_name] != 0:
+        precision += tp[class_name] / (tp[class_name] + fp[class_name])
+      else:
+        precision += 1
+
+      if tp[class_name] + fn[class_name] != 0:
+        recall += tp[class_name] / (tp[class_name] + fn[class_name])
+      else:
+        recall += 1
+
       accuracy += (tp[class_name] + tn[class_name]) / (tp[class_name] + tn[class_name] + fn[class_name] + fp[class_name])
 
     precision /= len(self._class_names)
@@ -98,9 +129,18 @@ def NAryClassifier:
     tn_sum = sum(tn.values())
     fn_sum = sum(fn.values())
 
-    precision = tp_sum / (tp_sum + fp_sum)
-    recall = tp_sum / (tp_sum + fn_sum)
+    if tp_sum + fp_sum != 0:
+      precision = tp_sum / (tp_sum + fp_sum)
+    else:
+      precision = 1.0
+
+    if tp_sum + fn_sum != 0:
+      recall = tp_sum / (tp_sum + fn_sum)
+    else:
+      recall = 1.0
+
     accuracy = (tp_sum + tn_sum) / (tp_sum + fn_sum + tn_sum + fp_sum)
+
     f1 = 2.0 * precision * recall / (precision + recall)
 
     micro = {'precision' : precision,
@@ -109,3 +149,19 @@ def NAryClassifier:
              'f1'        : f1}
 
     return {'micro' : micro, 'macro' : macro}
+
+
+def main():
+  class_to_ids = {'c' : [1, 2, 3], 'j' : [4]}
+  id_to_text = {1: ["chinese", "beijing", "chinese"],
+                2: ["chinese", "chinese", "shangai"],
+                3: ["chinese", "macao"],
+                4: ["tokyo", "japan", "chinese"]}
+  txt = ["chinese", "chinese", "chinese", "tokyo", "japan"]
+
+  classifier = NAryClassifier(class_to_ids, id_to_text)
+  print(classifier.get_classes_for_text(txt))
+  test_set = [(txt, ["c"])]
+  print(classifier.get_metrics(test_set))
+
+main()
